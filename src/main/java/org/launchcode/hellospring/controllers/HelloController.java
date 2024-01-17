34d19926c77bbffc8f6c907581635e1d986f344c
Kevin Wiegman <wiegmank@gmail.com>
2024-01-16 21:08:07 -0600

    hello-spring up to conditionals

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,49 +1,63 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 
 @Controller
-@ResponseBody
+
 @RequestMapping("hello")
 public class HelloController {
 
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
     @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "! - path parameter";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) { return "Hello, " + name + "!"; }
 
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = '/hello/hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<select name='language'>" +
-                "<option value='1'>English</option>" +
-                "<option value='2'>French</option>" +
-                "<option value='3'>Spanish</option>" +
-                "<option value='4'>German</option>" +
-                "<option value='5'>Italian</option>" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
+//        return "<html>" +
+//                "<body>" +
+//                "<form action = '/hello/hello' method = 'post'>" + // submit a request to /hello
+//                "<input type = 'text' name = 'name' >" +
+//                "<select name='language'>" +
+//                "<option value='1'>English</option>" +
+//                "<option value='2'>French</option>" +
+//                "<option value='3'>Spanish</option>" +
+//                "<option value='4'>German</option>" +
+//                "<option value='5'>Italian</option>" +
+//                "<input type = 'submit' value = 'Greet Me!' >" +
+//                "</form>" +
+//                "</body>" +
+//                "</html>";
     }
 
-    @RequestMapping(value="hello", method = RequestMethod.POST)
-    //@ResponseBody
-    public String myGreeting(@RequestParam String name, @RequestParam String language) {
-        return createMessage(name, language);
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello"; //returns "hello" template
     }
+//    //fancy solution
+//    @RequestMapping(value="hello", method = RequestMethod.POST)
+//    @ResponseBody
+//    public String myGreeting(@RequestParam String name, @RequestParam String language) {
+//        return createMessage(name, language);
+//    }
 
     public static String createMessage(String name, String language) {
         String sayHello = "";
@@ -59,6 +73,17 @@ public class HelloController {
             sayHello = "Ciao";
         }
         return sayHello + " " + name + "!";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Launchcode");
+        names.add("Java");
+        names.add("JavaScript");
+
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 }
